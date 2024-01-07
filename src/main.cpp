a1c2570e7e52f8f3f30c04528c226b761bedf5f2
Gunzette <totallycisgenderUwU@gmail.com>
2024-01-07 20:23:29 +0100

    Testing driving back and forth

--- src/main.cpp
+++ src/main.cpp
@@ -8,21 +8,29 @@ void setup()
 {
     // join I2C bus (I2Cdev library doesn't do this automatically)
     Wire.begin();
-    Serial.begin(9600);
     motor.init();
+    delay(2000);
 }
 
 void loop()
 {
-    // drive 2 dc motors at speed=255, clockwise
-    Serial.println("run at speed=255");
-    motor.dcMotorRun(MOTOR_CHA, 100);
-    motor.dcMotorRun(MOTOR_CHB, -100);
-    delay(2000);
+    // Nach vorne Fahren
+    motor.dcMotorRun(MOTOR_CHA, -150);
+    motor.dcMotorRun(MOTOR_CHB, -200);
+    delay(750);
     
-    // stop 2 motors
-    Serial.println("stop");
+    // stop 2 motors;
     motor.dcMotorStop(MOTOR_CHA);
     motor.dcMotorStop(MOTOR_CHB);
-    delay(1000);
+    delay(750);
+
+    // Nach hinten Fahren
+    motor.dcMotorRun(MOTOR_CHA, 150);
+    motor.dcMotorRun(MOTOR_CHB, 200);
+    delay(750);
+    
+    // stop 2 motors
+    motor.dcMotorStop(MOTOR_CHA);
+    motor.dcMotorStop(MOTOR_CHB);
+    delay(750);
 }
